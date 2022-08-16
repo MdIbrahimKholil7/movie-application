7a057f0ea6321d02e30019fb8ae7fe4bd6b2dd7e
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2022-08-16 17:28:31 +0600

    responsive header complete

--- src/components/Share/Header.js
+++ src/components/Share/Header.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import logo from '../../assets/logo.png'
 const Header = () => {
     return (
         <div>
-            <Navbar bg="light" variant="light py-3">
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
                 <Container>
-                    <Navbar.Brand href=""><img src={logo} alt="logoImage" /></Navbar.Brand>
-                    <Nav className="fw-bold text-dark">
-                        <Nav.Link href="" className='text-dark fs-5'>Home</Nav.Link>
-                        <Nav.Link href="" className='text-dark fs-5'>All Movie</Nav.Link>
-                        <Nav.Link href="" className='text-dark fs-5'>Top Rated</Nav.Link>
-                    </Nav>
+                    <Navbar.Brand href="#home"><img src={logo} alt="logo" /></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                       
+                        <Nav className='ms-auto '>
+                            <Nav.Link className='text-dark fs-5 fw-bold'>Home</Nav.Link>
+                            <Nav.Link className='text-dark fs-5 fw-bold'>All Movie </Nav.Link>
+                            <Nav.Link className='text-dark fs-5 fw-bold'>Top Rated </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
         </div>
