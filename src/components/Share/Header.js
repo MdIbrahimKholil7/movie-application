67bfc20ff7c1a517b22dabc56fa951b181bcde65
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2022-08-17 19:48:44 +0600

    logo animate add

--- src/components/Share/Header.js
+++ src/components/Share/Header.js
@@ -1,16 +1,46 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import logo from '../../assets/logo.png'
+import {motion} from 'framer-motion'
 const Header = () => {
+    const imgVariants={
+        open:{
+            y:0,
+            transition:{
+                delay:0.4,
+                duration:0.5,
+                ease: "linear" ,
+                type:'spring',
+                stiffness:120
+            }
+        },
+        close:{
+            y:-112,
+            transition:{
+                delay:0.4,
+                duration:0.5,
+                ease: "linear" ,
+                type:'spring',
+                stiffness:120
+            }
+        }
+    }
     return (
         <div>
             <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
                 <Container>
-                    <Navbar.Brand href="#home"><img src={logo} alt="logo" /></Navbar.Brand>
+                    <Navbar.Brand href="#home">
+                        <motion.div
+                        variants={imgVariants}
+                        animate='open'
+                        initial='close'
+                        >
+                            <img src={logo} alt="logo" />
+                        </motion.div>
+                    </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
-                       
+
                         <Nav className='ms-auto '>
                             <Nav.Link className='text-dark fs-5 fw-bold'>Home</Nav.Link>
                             <Nav.Link className='text-dark fs-5 fw-bold'>All Movie </Nav.Link>
