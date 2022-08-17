db5f2b4679ffb625b5f6c2d56595efccbb687460
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2022-08-17 11:59:31 +0600

    card design complete

--- src/components/Pages/MovieCard.js
+++ src/components/Pages/MovieCard.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import '../Style/MovieCard.css'
-const MovieCard = ({movie,index,setIndex}) => {
-    console.log(movie)
+const MovieCard = ({movie,index,movieIndex}) => {
+    console.log(index===movieIndex  )
+    console.log('index',index,'set',movieIndex  )
     const {image:{original},name,language,rating,schedule:{time}}=movie.show || {}
     return (
-        <div>
-            <div className='card-container'>
+        <div className='d-flex justify-content-center align-items-center'>
+            <div className={` card-container  ${ index === movieIndex ? 'active-card hover-card':'inactive-card'}`}>
                 <div className='movie-card'>
                     <img className='img' src={original} alt={name} />
                     <div className='movie-name'>
-                        <h3 className='fs-4'>Movie Name:{name}</h3>
+                       
                         <h3 className='fs-5'>Category:{language} Movie</h3>
                     </div>
                 </div>
